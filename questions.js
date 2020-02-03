ddf3986c712783d460b2098a85984f312513b6cd
Jesse Gooroovadoo <mjgooroovadoo@hotmail.com>
2020-02-03 22:14:02 +1100

    .

---
+++ questions.js
@@ -0,0 +1,34 @@
+
+//Questions will reflect the show "The Office"
+
+var questions = [
+    {
+        title: "Who does Michael accidentally hit with his car in the parking lot?",
+        choices: ["Meredith", "Phyllis", "Kelly", "Pam"],
+        answer: "Meredith"
+    },
+    {
+        title: "What does Michael pick as his username when he signs up for an online dating site?",
+        choices: ["PrisonMike", "LittleKidLover", "IAmTheBoss", "MichaelScarn"],
+        answer: "LittleKidLover"
+    },
+    {
+        title: "According to 'Prison Mike', what is the worst thing about prison?",
+        choices: ["the orcs", "The dementors", "Tyrone", "The banshees"],
+        answer: "The dementors"
+    },
+    {
+        title: "On the 'Diversity Day' episode, what stand up comedian does Michael Scott imitate?",
+        choices: ["Chris Rock", "Richard Pryor", "Eddie Murphy", "Kevin Hart"],
+        answer: "Chris Rock"
+    },
+    {
+        title: "What's in the giant pot that Kevin drops all over the office floor?",
+        choices: ["gravy", "chili", "chicken noodle soup", "hot water"],
+        answer: "chili"
+    },
+
+
+];
+//Just checking my console log :)
+console.log(questions);
